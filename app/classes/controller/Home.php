21a1a6ee15d1866397ee20f8ce17d564d1476dd6
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-29 11:32:03 +0300

    nauja struktura su Controller

--- app/classes/controller/Home.php
+++ app/classes/controller/Home.php
@@ -7,6 +7,8 @@ class Home extends \Core\Page\Controller {
     public function __construct() {
         parent::__construct();
         $this->page['content'] = 'Kregzdutes eina na';
+        $view = new \Core\Page\View(['title' => 'Siandien miegam']);
+        $this->page['content'] = $view->render(ROOT_DIR . '/app/views/content.tpl.php');
     }
 
 }
