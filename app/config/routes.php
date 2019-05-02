e348824b7bbef19bee9afeaf70bc885a302fc6e7
Katamaranas <ernestas.zidokas@gmail.com>
2019-05-02 10:43:51 +0300

    Router class commit

---
+++ app/config/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+\Core\Page\Router::addRoute('/about', '\App\Controller\About');
+\Core\Page\Router::addRoute('/home', '\App\Controller\Home');
+
--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,5 @@
 <?php
-
+    
 require_once '../bootloader.php';
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/about.php':
-        $controller = '\App\Controller\About';
-        break;
-    default :
-        $controller = '\App\Controller\Home';
-        break;
-}
-
-$control = new $controller();
-print $control->onRender();
-
-
+$app->run();
